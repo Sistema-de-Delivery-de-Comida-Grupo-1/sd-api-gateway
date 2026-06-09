077f7536db4fc0228f242a1bceadc6e655c604c0
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-06-09 16:26:37 -0300

    init

---
+++ src/main/java/br/ifg/urutai/sdapigateway/SdApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.urutai.sdapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdApiGatewayApplication.class, args);
+    }
+
+}
